1013047d988c2beddcb89ff7e9a14aea2df9a8a2
Andy Piper <apiper@twitter.com>
2017-06-27 15:43:00 +0100

    restructure for cocoapods

---
+++ Pods/Target Support Files/Pods-TweetJSON-TweetJSONUITests/Pods-TweetJSON-TweetJSONUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TweetJSON_TweetJSONUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TweetJSON_TweetJSONUITests
+@end
---
+++ Pods/Target Support Files/Pods-TweetJSON/Pods-TweetJSON-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TweetJSON : NSObject
+@end
+@implementation PodsDummy_Pods_TweetJSON
+@end
